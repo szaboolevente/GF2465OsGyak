c93dfe7667b51c537f3a4df9305bef9085bafe0a
szaboolevente <73876687+szaboolevente@users.noreply.github.com>
2021-04-20 11:54:25 +0200

    Update and rename 11 to child.c

---
+++ GF2465_0309/child.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main (void) {
+int i = 0;
+
+for(i = 0; i < 5; i++){
+	printf("Szabo Levente, GF2465, GE-IK\n");
+}
+
+return 0;
+
+}
