478ee42e2368ec7f7a5776ee3ec1f71871495823
szaboolevente <73876687+szaboolevente@users.noreply.github.com>
2022-05-11 23:30:51 +0200

    Create GF2465_4fel.c

---
+++ 2. Gyakorlat/GF2465_4fel.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main() {
+    pid_t parent_pid;
+    pid_t child_pid;
+    int pid, status;
+
+    parent_pid = getpid();
+    printf("A szülő process ID: %d\n", parent_pid);
+
+    if((pid = fork())==0) {
+        child_pid = getpid();
+        printf("A gyerek process ID: %d\n", child_pid);
+        status = execlp("ls", "ls", "-l", (char *)0);
+		if (status == -1)
+		{	perror("Hiba történt a gyerek futtatáskor.\n");
+			exit (1);
+		}        
+    }
+	printf("A szülő (%d) vár a gyermek (%d) befejeződésére.\n", parent_pid, child_pid);
+	if (pid != wait(&status)) {
+        perror(" Szülő - várakozási hiba ");
+    }
+
+    printf("A gyerek processnek vége.\n");
+    printf("A szülő process ID: %d\n", parent_pid);
+    
+    return 0;
+}
