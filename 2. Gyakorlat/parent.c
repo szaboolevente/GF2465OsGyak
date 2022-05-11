5c6988a3e6e739b78502eafa5a9b5e3191fb789c
szaboolevente <73876687+szaboolevente@users.noreply.github.com>
2022-05-11 22:58:01 +0200

    Update parent.c

--- 2. Gyakorlat/parent.c
+++ 2. Gyakorlat/parent.c
@@ -1,33 +1,3 @@
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-int main() {
-    pid_t parent_pid;
-    pid_t child_pid;
-    int pid, status;
-
-    parent_pid = getpid();
-    printf("A szulo process ID: %d\n", parent_pid);
-
-    if((pid = fork())==0) {
-        child_pid = getpid();
-        printf("A gyerek process ID: %d\n", child_pid);
-        status = execl("./child", "./child", (char *)0);
-		if (status == -1)
-		{	perror("Hiba tortent a gyerek processz futtatasakor.\n");
-			exit (1);
-		}        
-    }
-	printf("A szulo processz (%d) var a gyerek processz (%d) befejezesere.\n", parent_pid, child_pid);
-	if (pid != wait(&status)) {
-        perror("Szulo processz - varakozasi hiba ");
-    }
-
-    printf("A gyerek processnek#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
